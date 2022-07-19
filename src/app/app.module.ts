58028eea05fdcbb862f4be50646212a862ec4212
Pedro-s10 <pedrorxrxx@gmail.com>
2022-07-19 15:26:29 -0300

    🚩 using mat-toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppComponent } from './app.component';
 import { AgentDetailComponent } from './agent-detail/agent-detail.component';
 import { AgentsComponent } from './agents/agents.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, AgentsComponent, AgentDetailComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
