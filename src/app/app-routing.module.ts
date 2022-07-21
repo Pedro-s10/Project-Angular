47a8a17d95a0b8fac9e89f7513a2115005478959
Pedro-s10 <pedrorxrxx@gmail.com>
2022-07-21 14:49:07 -0300

    ✅ Add Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AgentsComponent } from './agents/agents.component';
+
+const routes: Routes = [
+  { path: 'agents', component: AgentsComponent }
+];
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
