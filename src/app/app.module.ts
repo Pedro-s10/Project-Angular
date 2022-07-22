0830ee308f211e5820721e0dfa320647d9243e7f
Pedro-s10 <pedrorxrxx@gmail.com>
2022-07-22 16:31:03 -0300

    ✨ introducing new folders and files to import angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,28 @@ import { AppComponent } from './app.component';
 import { AgentDetailComponent } from './agent-detail/agent-detail.component';
 import { AgentsComponent } from './agents/agents.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MessagesComponent } from './messages/messages.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
-import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatTooltipModule} from '@angular/material/tooltip';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [AppComponent, AgentsComponent, AgentDetailComponent, MessagesComponent, DashboardComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatExpansionModule, AppRoutingModule,MatIconModule, FlexLayoutModule, MatTooltipModule, MatCardModule, MatListModule],
+  declarations: [
+    AppComponent,
+    AgentsComponent,
+    AgentDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
