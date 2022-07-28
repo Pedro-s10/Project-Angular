b40d806bfa327caeaaeb4c92f18ac3b06e319515
Pedro-s10 <pedrorxrxx@gmail.com>
2022-07-28 15:15:22 -0300

    🔥 removing StyleUrls from components messages and toolbar is not using

--- src/app/core/components/messages/messages.component.ts
+++ src/app/core/components/messages/messages.component.ts
@@ -4,7 +4,7 @@ import { MessageService } from '../../services/message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+   // styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent {
 
